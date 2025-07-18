34d620e557dacab0d4aac660a19cf6cd99ca5149
educationED <porriterzip@gmail.com>
2025-07-18 12:17:26 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const wikiData = [
+    {
+        id: "afganistan",
+        title: "Afganistan",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Afghanistan.svg", // Örnek bayrak URL'si
+        content: `
+            <p>Afganistan (Farsça: افغانستان, Afġānistān), resmî adıyla Afganistan İslam Emirliği, Orta Asya ve Güney Asya'nın kesişim noktasında bulunan bir ülkedir.</p>
+            <p>Başkent: Kâbil</p>
+            <p>Resmi Diller: Peştuca, Darice</p>
+            <p>Devlet Başkanı (2025 itibarıyla): Aktif bir hükümet başkanı yerine Taliban Lideri kullanılmaktadır. (Bu bilgi değişebilir, güncel bilgi girilmelidir)</p>
+        `
+    },
+    {
+        id: "adolf-hitler",
+        title: "Adolf Hitler",
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/ea/Adolf_Hitler_portrait_1925.jpg", // Örnek resim URL'si
+        content: `
+            <p>Adolf Hitler (20 Nisan 1889 – 30 Nisan 1945), Avusturya doğumlu Alman politikacı ve Nazi Partisi'nin lideriydi.</p>
+            <p>Görevi: Almanya Şansölyesi (1933–1945), Führer (1934–1945)</p>
+            <p>Parti: Nazi Partisi</p>
+        `
+    },
+    {
+        id: "turkiye",
+        title: "Türkiye",
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg",
+        content: `
+            <p>Türkiye (resmî adıyla Türkiye Cumhuriyeti), büyük ölçüde Batı Asya'da, küçük bir kısmı ise Güneydoğu Avrupa'da yer alan bir ülkedir.</p>
+            <p>Başkent: Ankara</p>
+            <p>Resmi Dil: Türkçe</p>
+            <p>Devlet Başkanı (2025 itibarıyla): Recep Tayyip Erdoğan</p>
+        `
+    }
+    // Daha fazla maddeyi buraya ekleyebilirsiniz
+];
